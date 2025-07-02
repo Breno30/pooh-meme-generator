512455ff83a6498c252b99efa173c9778c261e7e
breno30 <brenodonascimentosilvaa@gmail.com>
2025-07-02 20:51:38 -0300

    refactor: update Terraform resources for AWS Lambda and API Gateway names

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -1,75 +1,79 @@
-resource "aws_iam_role" "example" {
-  name = "test_role"
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "service-lambda-execution-role"
 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
     ]
   })
 
   tags = {
-    tag-key = "tag-value"
+    Project     = "PoohMeme"
+    Environment = "Development"
   }
 }
 
-resource "aws_api_gateway_rest_api" "api_gateway_example" {
+resource "aws_api_gateway_rest_api" "service_api_gateway" {
   body = jsonencode({
     openapi = "3.0.1"
     info = {
-      title   = "example"
+      title   = "PoohMemeAPI"
       version = "1.0"
     }
   })
 
-  name = "example"
-
+  name        = "pooh-meme-api"
+  description = "API Gateway for pooh meme generator"
 }
 
-resource "aws_lambda_function" "example_function" {
+resource "aws_lambda_function" "service_lambda_function" {
   filename      = "lambda.zip"
-  function_name = "example_function"
-  role          = aws_iam_role.example.arn
+  function_name = "pooh-meme-lambda-function"
+  role          = aws_iam_role.lambda_execution_role.arn
   handler       = "index.lambda_handler"
   runtime       = "python3.12"
+
+  tags = {
+    Project     = "PoohMeme"
+    Environment = "Development"
+  }
 }
 
-# -----------
-
-resource "aws_api_gateway_resource" "example_resource" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway_example.id
-  parent_id   = aws_api_gateway_rest_api.api_gateway_example.root_resource_id
-  path_part   = "your_path"  # e.g., "items" or "users"
+resource "aws_api_gateway_resource" "api_path_resource" {
+  rest_api_id = aws_api_gateway_rest_api.service_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.service_api_gateway.root_resource_id
+  path_part   = "generate"
 }
 
-resource "aws_api_gateway_method" "example_method" {
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway_example.id
-  resource_id   = aws_api_gateway_resource.example_resource.id 
+resource "aws_api_gateway_method" "post_api_method" {
+  rest_api_id   = aws_api_gateway_rest_api.service_api_gateway.id
+  resource_id   = aws_api_gateway_resource.api_path_resource.id
   http_method   = "POST"
   authorization = "NONE" # You can change this based on your authorization needs
 }
 
-resource "aws_api_gateway_integration" "lambda_proxy" {
-  rest_api_id             = aws_api_gateway_rest_api.api_gateway_example.id
-  resource_id             = aws_api_gateway_resource.example_resource.id 
-  http_method             = aws_api_gateway_method.example_method.http_method 
+resource "aws_api_gateway_integration" "lambda_proxy_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.service_api_gateway.id
+  resource_id             = aws_api_gateway_resource.api_path_resource.id
+  http_method             = aws_api_gateway_method.post_api_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.example_function.invoke_arn
+  uri                     = aws_lambda_function.service_lambda_function.invoke_arn
 }
 
-resource "aws_lambda_permission" "api_gateway_lambda_permission" {
+resource "aws_lambda_permission" "api_gateway_invoke_lambda_permission" {
   statement_id  = "AllowAPIGatewayInvokeLambda"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.example_function.function_name
+  function_name = aws_lambda_function.service_lambda_function.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.api_gateway_example.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.service_api_gateway.execution_arn}/*/*"
 }
 

1c516e53ba7c52a7772bcdca250700ec48bccd17
breno30 <brenodonascimentosilvaa@gmail.com>
2025-07-09 19:51:08 -0300

    remove unnecessary api gateway

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -65,19 +65,6 @@ resource "aws_iam_role_policy_attachment" "lambda_attach_bedrock_policy" {
   policy_arn = aws_iam_policy.bedrock_invoke_policy.arn
 }
 
-resource "aws_api_gateway_rest_api" "service_api_gateway" {
-  body = jsonencode({
-    openapi = "3.0.1"
-    info = {
-      title   = "PoohMemeAPI"
-      version = "1.0"
-    }
-  })
-
-  name        = "pooh-meme-api"
-  description = "API Gateway for pooh meme generator"
-}
-
 # Archive lambda function code
 data "archive_file" "lambda" {
   type        = "zip"
@@ -98,36 +85,6 @@ resource "aws_lambda_function" "service_lambda_function" {
   }
 }
 
-resource "aws_api_gateway_resource" "api_path_resource" {
-  rest_api_id = aws_api_gateway_rest_api.service_api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.service_api_gateway.root_resource_id
-  path_part   = "generate"
-}
-
-resource "aws_api_gateway_method" "post_api_method" {
-  rest_api_id   = aws_api_gateway_rest_api.service_api_gateway.id
-  resource_id   = aws_api_gateway_resource.api_path_resource.id
-  http_method   = "POST"
-  authorization = "NONE" # You can change this based on your authorization needs
-}
-
-resource "aws_api_gateway_integration" "lambda_proxy_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.service_api_gateway.id
-  resource_id             = aws_api_gateway_resource.api_path_resource.id
-  http_method             = aws_api_gateway_method.post_api_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.service_lambda_function.invoke_arn
-}
-
-resource "aws_lambda_permission" "api_gateway_invoke_lambda_permission" {
-  statement_id  = "AllowAPIGatewayInvokeLambda"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.service_lambda_function.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.service_api_gateway.execution_arn}/*/*"
-}
 
 resource "aws_lambda_function_url" "lambda_function_url" {
   function_name = aws_lambda_function.service_lambda_function.function_name
@@ -170,10 +127,6 @@ resource "aws_s3_bucket_object" "object" {
 
 output "bucket_name" {
   value = aws_s3_bucket.project_bucket.id
-}
-
-output "lambda_function_endpoint" {
-  value = aws_api_gateway_integration.lambda_proxy_integration.rest_api_id
 }
 
 output "lambda_function_url" {

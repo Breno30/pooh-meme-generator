e9d45df93acf1329ea2e1ce24112f013b90f3d03
breno30 <brenodonascimentosilvaa@gmail.com>
2025-07-10 21:08:27 -0300

    fix: update S3 bucket policy to include Sid for public read access

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -137,12 +137,13 @@ resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.project_bucket.arn}/*"
-      }
+        {
+          Sid = "PublicReadGetObject",
+          Effect = "Allow",
+          Principal = "*",
+          Action = "s3:GetObject",
+          Resource = "${aws_s3_bucket.project_bucket.arn}/*"
+        }
     ]
   })
 }

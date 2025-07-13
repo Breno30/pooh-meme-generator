d144cb875bf202fd3d01143559367e1d765e20bc
breno30 <brenodonascimentosilvaa@gmail.com>
2025-07-13 15:26:39 -0300

    correct local variable values

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -47,9 +47,9 @@ provider "aws" {
 locals {
   project_hash_result    = random_string.project_hash.result
   dynamodb_table_name    = "${var.project_name}-${local.project_hash_result}"
-  lambda_function_name   = "${var.project_name}-lambda-function-${local.project_hash_result}"
-  iam_role_name          = "service-lambda-execution-role-${local.project_hash_result}"
-  iam_policy_name        = "lambda-bedrock-invoke-policy-${local.project_hash_result}"
+  lambda_function_name   = "lambda-function-${var.project_name}-${local.project_hash_result}"
+  iam_role_name          = "lambda-execution-role-${var.project_name}-${local.project_hash_result}"
+  iam_policy_name        = "lambda-bedrock-invoke-policy-${var.project_name}-${local.project_hash_result}"
   s3_bucket_name         = "${var.project_name}-${local.project_hash_result}"
 }
 

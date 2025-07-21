6407bf0523e4a8627c5bd430b459620b7b6af24a
breno30 <brenodonascimentosilvaa@gmail.com>
2025-07-21 19:59:13 -0300

    adjust variables default value

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,19 +1,23 @@
 variable "aws_region" {
-  description = "A prefix to use for naming resources, like 'my-project'."
+  description = "The AWS region where resources will be deployed (e.g., 'us-east-1')."
   type        = string
+  default     = "us-east-1"
 }
 
 variable "project_name" {
-  description = "A prefix to use for naming resources, like 'my-project'."
+  description = "A unique name for the project, used as a prefix for naming resources (e.g., 'my-app')."
   type        = string
+  default     = "my-app"
 }
 
 variable "lambda_memory_size" {
-  description = "A prefix to use for naming resources, like 'my-project'."
+  description = "The amount of memory (in MB) allocated to the Lambda function."
   type        = number
+  default     = 1024 # Default to 1024 MB
 }
 
 variable "lambda_timeout" {
-  description = "A prefix to use for naming resources, like 'my-project'."
+  description = "The maximum amount of time (in seconds) that the Lambda function can run."
   type        = number
+  default     = 10 # Default to 10 seconds
 }

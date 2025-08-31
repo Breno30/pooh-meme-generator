255c9565ba8c84f17e1621a0973850b1c7750c0d
breno30 <brenodonascimentosilvaa@gmail.com>
2025-08-31 16:34:07 -0300

    fix: set default AWS region in variables.tf

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "aws_region" {
   description = "The AWS region where resources will be deployed (e.g., 'us-east-1')."
   type        = string
+  default     = "us-east-1"
 }
 
 variable "project_name" {

9fc3745a2bfcfb3c782e82f6fa4ba7d815312d04
breno30 <brenodonascimentosilvaa@gmail.com>
2026-03-05 22:49:05 -0300

    remove unnecessary lines

--- src/lambda/pooh_meme_generator/app.py
+++ src/lambda/pooh_meme_generator/app.py
@@ -3,8 +3,6 @@ import boto3
 import os
 from botocore.exceptions import ClientError
 
-
-
 # --- Environment Variables ---
 MODEL_ID = os.environ.get('MODEL_ID')
 APP_REGION = os.environ.get('APP_REGION')

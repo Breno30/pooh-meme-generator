a218c3c9ef8f46b813e8805c99b8c383b43e0fc6
breno30 <brenodonascimentosilvaa@gmail.com>
2026-03-11 22:41:42 -0300

    style: format code for consistency in lambda.tf

--- deploy/terraform/lambda.tf
+++ deploy/terraform/lambda.tf
@@ -5,22 +5,22 @@ data "archive_file" "lambda" {
 }
 
 resource "aws_lambda_function" "service_lambda_function" {
-  depends_on    = [data.archive_file.lambda]
-  filename      = "temp/lambda.zip"
-  function_name = local.lambda_function_name
+  depends_on       = [data.archive_file.lambda]
+  filename         = "temp/lambda.zip"
+  function_name    = local.lambda_function_name
   source_code_hash = data.archive_file.lambda.output_base64sha256
-  role          = aws_iam_role.lambda_execution_role.arn
-  handler       = "app.lambda_handler"
-  runtime       = "python3.12"
-  memory_size   = var.lambda_memory_size
-  timeout       = var.lambda_timeout
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "app.lambda_handler"
+  runtime          = "python3.12"
+  memory_size      = var.lambda_memory_size
+  timeout          = var.lambda_timeout
 
   environment {
     variables = {
-      APP_REGION = "${var.aws_region}"
-      TABLE_NAME = local.dynamodb_table_name
-      MODEL_ID   = var.model_id
-      PROVIDER   = var.model_provider
+      APP_REGION     = "${var.aws_region}"
+      TABLE_NAME     = local.dynamodb_table_name
+      MODEL_ID       = var.model_id
+      PROVIDER       = var.model_provider
       OPENAI_API_KEY = var.openai_api_key
     }
   }

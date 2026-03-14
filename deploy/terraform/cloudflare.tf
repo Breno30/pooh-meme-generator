06e2525619904b1bef1a4aae889145b4baf897c5
breno30 <brenodonascimentosilvaa@gmail.com>
2026-03-13 22:20:09 -0300

    correct resource name

--- deploy/terraform/cloudflare.tf
+++ deploy/terraform/cloudflare.tf
@@ -2,7 +2,7 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-resource "cloudflare_record" "cname_app" {
+resource "cloudflare_dns_record" "cname_app" {
   zone_id = var.cloudflare_zone_id
   name    = "meme"
   type    = "CNAME"
